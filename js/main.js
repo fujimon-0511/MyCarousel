e55f81117743f861d2866cc7a8aedb28644355e4
fujimon <95519718+fujimon-0511@users.noreply.github.com>
2022-01-16 16:30:18 +0900

    新規追加

---
+++ js/main.js
@@ -0,0 +1,69 @@
+'use strict'
+
+{
+  const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
+  const ul = document.querySelector('ul');
+  const slides = ul.children;
+  let dots = [];
+  let currentIndex = 0;
+
+  function updateButtons() {
+    prev.classList.remove('hidden');
+    next.classList.remove('hidden');
+    
+    if (currentIndex === 0) {
+      prev.classList.add('hidden');
+    }
+    if (currentIndex === slides.length - 1) {
+      next.classList.add('hidden');
+    }
+  }
+
+  function moveslides() {
+    // const slideWidth = slides[0].getBoundingClientRect().width;
+    // ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+    ul.style.transform = `translateX(${-100 * currentIndex}%)`;
+  }
+
+  function setupDots() {
+    for (let i = 0; i < slides.length; i++) {
+      const button = document.createElement('button');
+      button.addEventListener('click', () => {
+        currentIndex = i;
+        updateDots();
+        updateButtons();
+        moveslides();
+      });
+      dots.push(button);
+      document.querySelector('nav').appendChild(button);
+    }
+    dots[0].classList.add('current');
+  }
+  
+  function updateDots() {
+    dots.forEach(dot => dot.classList.remove('current'));
+    dots[currentIndex].classList.add('current');
+  }
+  
+  updateButtons();
+  setupDots();
+
+  next.addEventListener('click', () =>{
+    currentIndex++;
+    updateButtons();
+    updateDots();
+    moveslides();
+  });
+  
+  prev.addEventListener('click', () =>{
+    currentIndex--;
+    updateButtons();
+    updateDots();
+    moveslides();
+  });
+
+  window.addEventListener('resize', () => {
+    moveslides();
+  });
+}
